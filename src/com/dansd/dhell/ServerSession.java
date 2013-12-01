244b3343797953afd4b71ce790a630fd712dd9b7
Francisco Dans <fdansv@gmail.com>
2013-12-01 20:27:28 +0000

    session getter

--- src/com/dansd/dhell/ServerSession.java
+++ src/com/dansd/dhell/ServerSession.java
@@ -1,7 +1,12 @@
 package com.dansd.dhell;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class ServerSession {
-    HashMap sessionMap = new HashMap<String, Session>();
+    private Map sessionMap = new HashMap<String, Session>();
+
+    Session getSession(String token){
+        return (Session) sessionMap.get(token);
+    }
 }
--- src/com/dansd/dhell/Session.java
+++ src/com/dansd/dhell/Session.java
@@ -1,4 +1,15 @@
 package com.dansd.dhell;
 
+import static java.lang.System.*;
+
 public class Session {
+    private String sessionToken;
+    private int timeOfInitiation;
+
+    public Session(){
+        timeOfInitiation = (int) (currentTimeMillis() / 1000L);
+    }
+    protected void generateSessionToken(){
+
+    }
 }
