833fb4012099572fbc217c47d48469c4a242cb1f
Francisco Dans <fdansv@gmail.com>
2013-12-01 21:48:34 +0000

    add getter and setter for zeppelin

--- src/com/dansd/dhell/Zeppelin.java
+++ src/com/dansd/dhell/Zeppelin.java
@@ -1,15 +1,37 @@
 package com.dansd.dhell;
 
+import java.util.HashMap;
+
 public class Zeppelin {
 
     private final byte[] requestString;
+
+    public String getCommand() {
+        return command;
+    }
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+
+    public HashMap getPayload() {
+        return payload;
+    }
+
+    public void setPayload(HashMap payload) {
+        this.payload = payload;
+    }
+
+    private String command;
+    private HashMap payload;
 
     public Zeppelin(byte[] reqString) {
         this.requestString = reqString;
         decode();
     }
-
     private void decode() {
-
+        parseCommand();
+        parsePayload();
     }
+
 }
